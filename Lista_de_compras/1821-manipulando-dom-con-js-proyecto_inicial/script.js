d5faa6ebcaa8431f035211031c31194502e7ca4d
citagrs <smalqs@gmail.com>
2022-05-15 07:18:53 -0500

    curso03 completo

--- Lista_de_compras/1821-manipulando-dom-con-js-proyecto_inicial/script.js
+++ Lista_de_compras/1821-manipulando-dom-con-js-proyecto_inicial/script.js
@@ -6,12 +6,33 @@ const agregarProducto = (evento) => {
 
     /* Evita que la pagina se refresque al dar hacer click en el elemento boton */
     evento.preventDefault();
-    
+
     /* Añade a la constante input la informacion que se agrega en el input */
     const input = document.querySelector("[data-form-input]");
-    
+    const value = input.value;
 
-    console.log(input.value);
+    /* Añade a la constante lista la informacion del ul del input nombrado data-lista */
+    const lista = document.querySelector("[data-lista]")
+
+    /* Crea el elemento li */
+    const producto = document.createElement("li");
+    producto.classList.add("card");
+
+    /* Permite que despues de almacenar la palabra en la constante, el input quede en blanco */
+    input.value = "";
+
+    const contenido = `<div>
+            <i class="far fa-check-square icon"></i>
+            <span class="product">${value}</span>
+        </div>
+        <i class="fas fa-trash-alt trashIcon icon"></i>`;
+
+    /* Añade la infromacion de la variable contenido en la constante producto donde se almacena el
+    listado de los productos */    
+    producto.innerHTML = contenido;
+
+    lista.appendChild(producto);
+
 }
 
 /* Ahora se toma la constante btn y se llama el metodo addEventListener para "escuchar" el evento, el cual posee dos 
